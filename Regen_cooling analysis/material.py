d161bbc1a7c6771739ff6dbd9df3cd1b14927288
shripad02 <85055089+shripad02@users.noreply.github.com>
2026-06-23 12:48:16 +0530

    Update material.py

--- Regen_cooling analysis/material.py
+++ Regen_cooling analysis/material.py
@@ -4,8 +4,10 @@ class SS308L:
 
         self.k = 16.0
 
-    def conductivity(
-        self,
-        T=None
-    ):
+        self.max_outer_wall_temp = 600.0
+
+        self.max_inner_wall_temp = 1200.0
+
+    def conductivity(self, T=None):
+
         return self.k
